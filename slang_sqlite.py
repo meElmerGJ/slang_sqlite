515c63be01c67d9ac43255ec30ade3e38d5b52b4
ElmerGJ <elmerdaniel1120@gmail.com>
2022-06-27 20:36:56 -0500

    testing url conflit, adding project instructions

---
+++ slang_sqlite.py
@@ -0,0 +1,11 @@
+"""
+    Elaborar una aplicación de línea de comandos en Python que sirva cuyo propósito sea mantener un diccionario de
+    palabras del slang panameño (xopa, mopri, otras). Las palabras y su significado deben ser almacenadas dentro de una
+    base de datos SQLite. Las opciones dentro del programa deben incluir como mínimo:
+        a) Agregar nueva palabra
+        b) Editar palabra existente
+        c) Eliminar palabra existente
+        d) Ver listado de palabras
+        e) Buscar significado de palabra
+        f) Salir
+"""
